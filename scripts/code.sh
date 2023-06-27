e6869b4a0673ffb14175ac357fbabe6c1be0bbf4
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2023-06-27 14:06:28 -0400

    update scripts that install code and codium extensions

--- scripts/code.sh
+++ scripts/code.sh
@@ -1,5 +1,13 @@
-code --install-extension ms-python.python --force
-code --install-extension vscodevim.vim --force
-code --install-extension ms-vsliveshare.vsliveshare --force
-code --install-extension ms-python.vscode-pylance --force
-code --install-extension tabnine.tabnine-vscode --force
+$HOMEBREW_PREFIX/bin/code --install-extension asvetliakov.vscode-neovim
+$HOMEBREW_PREFIX/bin/code --install-extension ms-python.black-formatter
+$HOMEBREW_PREFIX/bin/code --install-extension ms-python.python
+$HOMEBREW_PREFIX/bin/code --install-extension ms-python.vscode-pylance
+$HOMEBREW_PREFIX/bin/code --install-extension ms-toolsai.jupyter
+$HOMEBREW_PREFIX/bin/code --install-extension ms-toolsai.jupyter-keymap
+$HOMEBREW_PREFIX/bin/code --install-extension ms-toolsai.jupyter-renderers
+$HOMEBREW_PREFIX/bin/code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+$HOMEBREW_PREFIX/bin/code --install-extension ms-toolsai.vscode-jupyter-slideshow
+$HOMEBREW_PREFIX/bin/code --install-extension quarto.quarto
+$HOMEBREW_PREFIX/bin/code --install-extension REditorSupport.r
+$HOMEBREW_PREFIX/bin/code --install-extension TabNine.tabnine-vscode
+$HOMEBREW_PREFIX/bin/code --install-extension ms-vsliveshare.vsliveshare --force
---
+++ scripts/codium.sh
@@ -0,0 +1,10 @@
+$HOMEBREW_PREFIX/bin/codium --install-extension asvetliakov.vscode-neovim
+$HOMEBREW_PREFIX/bin/codium --install-extension ms-python.python
+$HOMEBREW_PREFIX/bin/codium --install-extension ms-toolsai.jupyter
+$HOMEBREW_PREFIX/bin/codium --install-extension ms-toolsai.jupyter-keymap
+$HOMEBREW_PREFIX/bin/codium --install-extension ms-toolsai.jupyter-renderers
+$HOMEBREW_PREFIX/bin/codium --install-extension ms-toolsai.vscode-jupyter-cell-tags
+$HOMEBREW_PREFIX/bin/codium --install-extension ms-toolsai.vscode-jupyter-slideshow
+$HOMEBREW_PREFIX/bin/codium --install-extension quarto.quarto
+$HOMEBREW_PREFIX/bin/codium --install-extension REditorSupport.r
+$HOMEBREW_PREFIX/bin/codium --install-extension TabNine.tabnine-vscode
