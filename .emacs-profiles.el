3c42328f03c740d6f1432456da7dfdefe4b1f510
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2025-09-19 13:49:43 -0400

    use chemacs2 to run doom and spacemacs

---
+++ .emacs-profiles.el
@@ -0,0 +1,3 @@
+(("default"   . ((user-emacs-directory . "~/.config/emacs")))
+ ("doom"   . ((user-emacs-directory . "~/.config/emacs")))
+ ("space"   . ((user-emacs-directory . "~/.config/spacemacs"))))
