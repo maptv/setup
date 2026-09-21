cf373f784ae4635197a8f2b17e2b00166dd6a9f8
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2026-09-21 11:45:05 -0400

    make doom emacs start fullscreen (fullboth instead of maximum) and with large font

--- config.el
+++ config.el
@@ -21,6 +21,10 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Ewka Nerd Font" :size 24)
+      doom-big-font (font-spec :family "Ewka Nerd Font" :size 32))
+
+(add-to-list 'default-frame-alist '(fullscreen . fullboth))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
